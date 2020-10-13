fcbc00216de8459f4a7cf9e6950a3ca6b4d2ca0c
Ronnie Song <ronniesong0809@gmail.com>
2020-10-13 14:48:15 -0700

    feat: update mouselock, tankcontrols, and more

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float maximumY = 80F;
+    public float minimumY = -30F;
+    public float rotationY = 0F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+    }
+}
---
+++ Assets/TankControls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControls : MonoBehaviour
+{
+    public float moveSpeed = 15f; 
+    public bool _isGrounded = true;
+    Vector3 _inputVector;
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _inputVector = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+    }
+    
+    void FixedUpdate()
+    {
+        Vector3 movement = moveSpeed * 10f * _inputVector.z * Time.fixedDeltaTime * transform.forward + moveSpeed * 10f * _inputVector.x * Time.fixedDeltaTime * transform.right;
+        rb.MovePosition(transform.position + movement * Time.fixedDeltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Entered");
+        if (collision.gameObject.CompareTag("Terrain"))
+        {
+            _isGrounded = true;
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Exited");
+        if (collision.gameObject.CompareTag("Terrain"))
+        {
+            _isGrounded = false;
+        }
+    }
+}
