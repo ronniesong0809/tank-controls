384f8d2e0739aab2ec7f9cd675794b0c0e69480d
Ronnie Song <ronniesong0809@gmail.com>
2020-10-19 14:52:51 -0700

    feat: add mouse lock/release, and more features

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -10,6 +10,18 @@ public class MouseLook : MonoBehaviour
     public float minimumY = -30F;
     public float rotationY = 0F;
 
+    public enum RotationAxes
+    {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float maximumX = 360F;
+    public float minimumX = -360F;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +31,34 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+            transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+        }
+
+        // Lock cursor
+        if (Input.GetMouseButton(0))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // Release cursor
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
     }
 }
